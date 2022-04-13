d9d308393686a505dd7e9f3387195c11761477e9
Andre Cruz <mariliaeandre.iba@hotmail.com>
2022-04-13 17:53:48 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/cursojava/webservices/entities/User.java
+++ src/main/java/com/cursojava/webservices/entities/User.java
@@ -1,13 +1,18 @@
 package com.cursojava.webservices.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity /*especificação do JPA, e não a implementação*/
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
@@ -19,6 +24,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client") //um para muitos, mapeado por client
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -69,6 +77,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders(){
+        return orders;
     }
 
     @Override
