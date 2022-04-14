49628e176c79063cca7a478e120ca70815c20b8d
Andre Cruz <mariliaeandre.iba@hotmail.com>
2022-04-14 19:31:56 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/cursojava/webservices/config/TestConfig.java
+++ src/main/java/com/cursojava/webservices/config/TestConfig.java
@@ -5,10 +5,12 @@ import java.util.Arrays;
 
 import com.cursojava.webservices.entities.Category;
 import com.cursojava.webservices.entities.Order;
+import com.cursojava.webservices.entities.OrderItem;
 import com.cursojava.webservices.entities.Product;
 import com.cursojava.webservices.entities.User;
 import com.cursojava.webservices.entities.enums.OrderStatus;
 import com.cursojava.webservices.repositories.CategoryRepository;
+import com.cursojava.webservices.repositories.OrderItemRepository;
 import com.cursojava.webservices.repositories.OrderRepository;
 import com.cursojava.webservices.repositories.ProductRepository;
 import com.cursojava.webservices.repositories.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -50,7 +55,7 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
-        p1.getCategories().add(cat2);//aqui acontece a associação de um produto com sua categoria
+        p1.getCategories().add(cat2);// aqui acontece a associação de um produto com sua categoria
         p2.getCategories().add(cat1);
         p2.getCategories().add(cat3);
         p3.getCategories().add(cat3);
@@ -68,6 +73,13 @@ public class TestConfig implements CommandLineRunner {
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 
 }
--- src/main/java/com/cursojava/webservices/entities/OrderItem.java
+++ src/main/java/com/cursojava/webservices/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.cursojava.webservices.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -14,7 +15,7 @@ import com.cursojava.webservices.entities.pk.OrderItemPK;
 public class OrderItem implements Serializable {
 
     @EmbeddedId
-    private OrderItemPK id;
+    private OrderItemPK id = new OrderItemPK();
     
     private Integer quantity;
     private Double price;
@@ -30,6 +31,7 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    @JsonIgnore
     public Order getOrder(){
         return id.getOrder();
     }
