549be7fc9a10b4fc5215ae19cbbf84797b899352
Andre Cruz <mariliaeandre.iba@hotmail.com>
2022-04-19 08:25:25 -0300

    Payment, one-to-one association

--- src/main/java/com/cursojava/webservices/config/TestConfig.java
+++ src/main/java/com/cursojava/webservices/config/TestConfig.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import com.cursojava.webservices.entities.Category;
 import com.cursojava.webservices.entities.Order;
 import com.cursojava.webservices.entities.OrderItem;
+import com.cursojava.webservices.entities.Payment;
 import com.cursojava.webservices.entities.Product;
 import com.cursojava.webservices.entities.User;
 import com.cursojava.webservices.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 
 }
